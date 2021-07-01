12ab922229872aa21cc25674daa76ca90a65bc2a
Andrew Gould <gould.andrew@gmail.com>
2021-07-01 18:22:08 +1000

    test: Adding test support via Jest, adding basic tests

---
+++ tests/app.spec.ts
@@ -0,0 +1,56 @@
+import { RobotState } from '../app/model'
+import { DirectionsToNumbers } from '../app/constants'
+import { Robot } from '../app/controller'
+import * as fs from 'fs'
+
+let state: RobotState = {
+    x: 0,
+    y: 0,
+    direction: DirectionsToNumbers.East,
+    placed: false,
+}
+
+const robot = new Robot({ state, debug: false })
+
+test('Scenario 1', () => {
+    const directions = ['PLACE 0,0,NORTH', 'MOVE', 'REPORT']
+    console.log = jest.fn()
+
+    directions.forEach(function (line) {
+        let action = line.split(' ')
+        robot.process(action)
+    })
+
+    expect(console.log).toBeCalledWith('0,1,NORTH')
+})
+
+test('Scenario 2', () => {
+    const directions = ['PLACE 0,0,NORTH', 'LEFT', 'REPORT']
+    console.log = jest.fn()
+
+    directions.forEach(function (line) {
+        let action = line.split(' ')
+        robot.process(action)
+    })
+
+    expect(console.log).toBeCalledWith('0,0,WEST')
+})
+
+test('Scenario 3', () => {
+    const directions = [
+        'PLACE 1,2,EAST',
+        'MOVE',
+        'MOVE',
+        'LEFT',
+        'MOVE',
+        'REPORT',
+    ]
+    console.log = jest.fn()
+
+    directions.forEach(function (line) {
+        let action = line.split(' ')
+        robot.process(action)
+    })
+
+    expect(console.log).toBeCalledWith('3,3,NORTH')
+})
